742e85b6a02eb6db90b7dbc5f8479e7a3cf55cde
Ivanzap <ivanzaporozhskiy@mail.ru>
2021-06-08 21:22:31 +0300

    HW1 opt add delete method

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -3,6 +3,7 @@ package ru.javawebinar.topjava.web;
 import org.slf4j.Logger;
 import ru.javawebinar.topjava.dao.MealDao;
 import ru.javawebinar.topjava.dao.MealDaoImpl;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.model.MealTo;
 
 import javax.servlet.ServletException;
@@ -19,6 +20,7 @@ import static ru.javawebinar.topjava.util.MealsUtil.filteredByStreams;
 public class MealServlet extends HttpServlet {
     private static final Logger log = getLogger(UserServlet.class);
     private static String MEALS = "/meals.jsp";
+    private static String INSERT_OR_EDIT = "/meal.jsp";
     private static final int CALORIES_PER_DAY = 2000;
 
     private MealDao dao;
@@ -35,11 +37,23 @@ public class MealServlet extends HttpServlet {
         List<MealTo> mealsTo;
         String action = request.getParameter("action");
 
-        if(action.equals("meals")) {
+        if(action.equalsIgnoreCase("meals")) {
             forward = MEALS;
             mealsTo = filteredByStreams(dao.getAllMeals(), LocalTime.MIN, LocalTime.MAX, CALORIES_PER_DAY);
             request.setAttribute("mealTo", mealsTo);
-            request.setAttribute("act", action);
+        } else if(action.equalsIgnoreCase("delete")) {
+            int id = Integer.parseInt(request.getParameter("id"));
+            dao.deleteMeal(id);
+            forward = MEALS;
+            mealsTo = filteredByStreams(dao.getAllMeals(), LocalTime.MIN, LocalTime.MAX, CALORIES_PER_DAY);
+            request.setAttribute("mealTo", mealsTo);
+        } else if(action.equalsIgnoreCase("edit")) {
+            int id = Integer.parseInt(request.getParameter("id"));
+            forward = INSERT_OR_EDIT;
+            Meal meal = dao.getMealById(id);
+            request.setAttribute("meal", meal);
+        } else {
+            forward = INSERT_OR_EDIT;
         }
 
         request.getRequestDispatcher(forward).forward(request, response);
