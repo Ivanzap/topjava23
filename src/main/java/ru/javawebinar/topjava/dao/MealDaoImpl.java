a02ff42798f7edfb29a9484d0c1ad87e9776bd34
Ivanzap <ivanzaporozhskiy@mail.ru>
2021-06-08 21:14:08 +0300

    HW1 opt add code into methods MealDaoImpl

--- src/main/java/ru/javawebinar/topjava/dao/MealDaoImpl.java
+++ src/main/java/ru/javawebinar/topjava/dao/MealDaoImpl.java
@@ -9,32 +9,40 @@ import java.util.Collections;
 import java.util.List;
 
 public class MealDaoImpl implements MealDao {
+    private static int MEAL_ID;
+
     List<Meal> meals;
 
     {
         meals = Collections.synchronizedList(new ArrayList<>());
-        meals.add(new Meal(1, LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500));
-        meals.add(new Meal(2, LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0), "Обед", 1000));
-        meals.add(new Meal(3, LocalDateTime.of(2020, Month.JANUARY, 30, 20, 0), "Ужин", 500));
-        meals.add(new Meal(4, LocalDateTime.of(2020, Month.JANUARY, 31, 0, 0), "Еда на граничное значение", 100));
-        meals.add(new Meal(5, LocalDateTime.of(2020, Month.JANUARY, 31, 10, 0), "Завтрак", 1000));
-        meals.add(new Meal(6, LocalDateTime.of(2020, Month.JANUARY, 31, 13, 0), "Обед", 500));
-        meals.add(new Meal(7, LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410));
+        meals.add(new Meal(++MEAL_ID, LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500));
+        meals.add(new Meal(++MEAL_ID, LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0), "Обед", 1000));
+        meals.add(new Meal(++MEAL_ID, LocalDateTime.of(2020, Month.JANUARY, 30, 20, 0), "Ужин", 500));
+        meals.add(new Meal(++MEAL_ID, LocalDateTime.of(2020, Month.JANUARY, 31, 0, 0), "Еда на граничное значение", 100));
+        meals.add(new Meal(++MEAL_ID, LocalDateTime.of(2020, Month.JANUARY, 31, 10, 0), "Завтрак", 1000));
+        meals.add(new Meal(++MEAL_ID, LocalDateTime.of(2020, Month.JANUARY, 31, 13, 0), "Обед", 500));
+        meals.add(new Meal(++MEAL_ID, LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410));
     }
 
     @Override
     public void addMeal(Meal meal) {
-
+        meals.add(meal);
     }
 
     @Override
     public void deleteMeal(int id) {
-
+        meals.remove(getMealById(id));
     }
 
     @Override
     public void updateMeal(Meal meal) {
-
+        int id = meal.getId();
+        Meal newMeal = getMealById(id);
+        newMeal.setDateTime(meal.getDateTime());
+        newMeal.setDescription(meal.getDescription());
+        newMeal.setCalories(meal.getCalories());
+        int index = meals.indexOf(getMealById(id));
+        meals.set(index, newMeal);
     }
 
     @Override
@@ -44,6 +52,10 @@ public class MealDaoImpl implements MealDao {
 
     @Override
     public Meal getMealById(int id) {
+        for (Meal meal : meals) {
+            if(meal.getId() == id)
+                return meal;
+        }
         return null;
     }
 }
