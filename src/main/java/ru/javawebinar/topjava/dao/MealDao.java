9866a250586efdc6d722e77c3142ffd2e1210601
Ivanzap <ivanzaporozhskiy@mail.ru>
2021-06-08 08:05:36 +0300

    HW1 opt add package 'dao'

---
+++ src/main/java/ru/javawebinar/topjava/dao/MealDao.java
@@ -0,0 +1,17 @@
+package ru.javawebinar.topjava.dao;
+
+import ru.javawebinar.topjava.model.Meal;
+
+import java.util.List;
+
+public interface MealDao {
+    public void addMeal(Meal meal);
+
+    public void deleteMeal(int id);
+
+    public void updateMeal(Meal meal);
+
+    public List<Meal> getAllMeals();
+
+    public Meal getMealById(int id);
+}
