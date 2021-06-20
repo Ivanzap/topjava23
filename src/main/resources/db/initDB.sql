112af61e8b7b984a4bc15fad466504271cba09e9
Ivanzap <ivanzaporozhskiy@mail.ru>
2021-06-20 17:05:53 +0300

    HW3 completed step 2

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,6 +1,8 @@
+DROP TABLE IF EXISTS meals;
 DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
+DROP SEQUENCE IF EXISTS global_seq_meals;
 
 CREATE SEQUENCE global_seq START WITH 100000;
 
@@ -23,3 +25,15 @@ CREATE TABLE user_roles
     CONSTRAINT user_roles_idx UNIQUE (user_id, role),
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
+
+CREATE SEQUENCE global_seq_meals START WITH 1;
+
+CREATE TABLE meals
+(
+    id              INTEGER PRIMARY KEY  DEFAULT nextval('global_seq_meals')         ,
+    user_meal_id    INTEGER REFERENCES   users(id),
+    date_time       TIMESTAMP             NOT NULL,
+    description     VARCHAR               NOT NULL,
+    calories        INTEGER               NOT NULL
+);
+CREATE UNIQUE INDEX user_unique_date_time ON meals (user_meal_id, date_time);
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,6 +1,8 @@
+DELETE FROM meals;
 DELETE FROM user_roles;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
+ALTER SEQUENCE global_seq_meals RESTART WITH 1;
 
 INSERT INTO users (name, email, password)
 VALUES ('User', 'user@yandex.ru', 'password'),
@@ -9,3 +11,14 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (user_meal_id, date_time, description, calories)
+VALUES (100000, '2021-06-19 09:00', 'Затрак user', 500),
+       (100000, '2021-06-19 12:30', 'Обед user', 1100),
+       (100000, '2021-06-19 18:30', 'Ужин user', 700),
+       (100000, '2021-06-20 08:30', 'Завтрак user', 400),
+       (100000, '2021-06-20 13:00', 'Обед user', 1000),
+       (100000, '2021-06-20 18:00', 'Ужин user', 450),
+       (100001, '2021-06-19 07:30', 'Завтрак admin', 600),
+       (100001, '2021-06-19 12:00', 'Обед admin', 1200),
+       (100001, '2021-06-19 19:00', 'Ужин admin', 400);
